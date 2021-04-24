f569244d82bd9e6c3e27e082b5c32cca096e0cbc
Lesley <lesleylai@lesleylai.info>
2021-04-23 18:03:04 -0600

    Update clang-format and clang-tidy settings

--- src/dummy.cpp
+++ src/dummy.cpp
@@ -5,8 +5,7 @@
 auto fib(int x) noexcept -> int
 {
   int a = 0, b = 1;
-  for (int i = 0; i < x; ++i) {
-    a = std::exchange(b, a + b);
-  }
+  for (int i = 0; i < x; ++i) { a = std::exchange(b, a + b); }
+
   return a;
 }
